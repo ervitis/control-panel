fca5174f595c61afed71df0e9652980e6eeffcee
ervitis <vitomarti@gmail.com>
2022-09-19 19:29:27 +0900

    feat: navbar style wip

--- front/src/components/recipes/NavBar/index.tsx
+++ front/src/components/recipes/NavBar/index.tsx
@@ -1,15 +1,47 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
 
 export const Component = () => {
     return (
-        <nav>
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-            <button>Login</button>
-            <button>Sign In</button>
-        </nav>
+        <ComponentWrapper>
+            <LinksWrapper>
+                <LinkWrapper to="/">Home</LinkWrapper>
+                <LinkWrapper to="/about">About</LinkWrapper>
+            </LinksWrapper>
+            <ControlAccessButtonsWrapper>
+                <LoginButtonWrapper type="button" onClick={() => {alert('hello')}}>Login</LoginButtonWrapper>
+                <SignInButtonWrapper type="button" onClick={() => alert('bye')}>Sign In</SignInButtonWrapper>
+            </ControlAccessButtonsWrapper>
+        </ComponentWrapper>
     )
 }
 
 Component.displayName = 'NavBar'
+
+const ComponentWrapper = styled.nav`
+    background-color: orange;
+    gap: 10px;
+    padding: 4px;
+    display: flex;
+`
+
+const LinksWrapper = styled.div`
+    margin: 0 10px;
+`
+
+const LinkWrapper = styled(Link)`
+    margin: 0 4px;
+`
+
+const ControlAccessButtonsWrapper = styled.div`
+    margin-left: auto;
+`
+
+const LoginButtonWrapper = styled.button`
+    background-color: gainsboro;
+`
+
+const SignInButtonWrapper = styled.button`
+    background-color: honeydew;
+`
